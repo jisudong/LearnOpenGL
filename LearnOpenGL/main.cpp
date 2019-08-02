7e39026a6c0aee2b01f98ed41c63ad0ab9a3a366
jisudong <jisudong@show-fitness.com>
2019-08-02 19:31:49 +0800

    init project

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by 秀健身 on 2019/8/2.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
