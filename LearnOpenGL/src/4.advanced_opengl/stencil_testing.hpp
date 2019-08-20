ea8607c472c9e7597f877f5e6001e2f95c5889df
jisudong <jisudong@show-fitness.com>
2019-08-20 20:14:58 +0800

    stencil testing

---
+++ LearnOpenGL/src/4.advanced_opengl/stencil_testing.hpp
@@ -0,0 +1,14 @@
+//
+//  stencil_testing.hpp
+//  LearnOpenGL
+//
+//  Created by 秀健身 on 2019/8/20.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#ifndef stencil_testing_hpp
+#define stencil_testing_hpp
+
+int stencil();
+
+#endif /* stencil_testing_hpp */
