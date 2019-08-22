135f3d7d6ccee2fc7affa461d88a470a472ca315
jisudong <jisudong@show-fitness.com>
2019-08-22 17:41:42 +0800

    face culling

---
+++ LearnOpenGL/src/4.advanced_opengl/face_culling.cpp
@@ -0,0 +1,239 @@
+//
+//  face_culling.cpp
+//  LearnOpenGL
+//
+//  Created by 秀健身 on 2019/8/22.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#include "face_culling.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.h"
+#include "camera.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "stb_image.h"
+#include <iostream>
+
+void facecull_framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void facecull_mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void facecull_scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+void facecull_processInput(GLFWwindow *window);
+unsigned int facecull_loadTexture(const char *path);
+
+const unsigned int FACECULL_SCR_WIDTH = 1280;
+const unsigned int FACECULL_SCR_HEIGHT = 720;
+
+Camera facecull_camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float facecull_lastX = (float)FACECULL_SCR_WIDTH / 2.0f;
+float facecull_lastY = (float)FACECULL_SCR_HEIGHT / 2.0f;
+bool facecull_firstMouse = true;
+
+float facecull_deltaTime = 0.0f;
+float facecull_lastFrame = 0.0f;
+
+int facecull() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+#endif
+    GLFWwindow *window = glfwCreateWindow(FACECULL_SCR_WIDTH, FACECULL_SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, facecull_framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, facecull_mouse_callback);
+    glfwSetScrollCallback(window, facecull_scroll_callback);
+    
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+    
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+    
+    Shader shader("./GLSL/4.advanced_opengl/3.1.blending.vs", "./GLSL/4.advanced_opengl/3.2.blending.fs");
+    
+    float cubeVertices[] = {
+        // Back face
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // Bottom-left
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // bottom-left
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+        // Front face
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+        // Left face
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-left
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-left
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-right
+        // Right face
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left
+        // Bottom face
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // top-left
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-left
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-right
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
+        // Top face
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left
+    };
+    
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glBindVertexArray(0);
+    
+    unsigned int textureID = facecull_loadTexture("./resources/textures/marble.jpg");
+    
+    shader.use();
+    shader.setInt("texture1", 0);
+    
+    while (!glfwWindowShouldClose(window)) {
+        double currentFrame = glfwGetTime();
+        facecull_deltaTime = currentFrame - facecull_lastFrame;
+        facecull_lastFrame = currentFrame;
+        
+        facecull_processInput(window);
+        
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        shader.use();
+        glm::mat4 projection = glm::perspective(glm::radians(facecull_camera.Zoom), (float)FACECULL_SCR_WIDTH / (float)FACECULL_SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = facecull_camera.getViewMatrix();
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, 1.0f));
+        shader.setMat4("model", model);
+        
+        glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    
+    glfwTerminate();
+    return 0;
+    
+}
+
+void facecull_framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void facecull_mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    if (facecull_firstMouse) {
+        facecull_lastX = xpos;
+        facecull_lastY = ypos;
+        facecull_firstMouse = false;
+    }
+    
+    double xoffset = xpos - facecull_lastX;
+    double yoffset = facecull_lastY - ypos;
+    facecull_lastX = xpos;
+    facecull_lastY = ypos;
+    
+    facecull_camera.processMouseMovement(xoffset, yoffset);
+}
+void facecull_scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
+{
+    facecull_camera.processMouseScroll(yoffset);
+}
+void facecull_processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+    
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        facecull_camera.processKeyboard(FORWARD, facecull_deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        facecull_camera.processKeyboard(BACKWARD, facecull_deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        facecull_camera.processKeyboard(LEFT, facecull_deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        facecull_camera.processKeyboard(RIGHT, facecull_deltaTime);
+    }
+}
+unsigned int facecull_loadTexture(const char *path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data) {
+        GLenum format;
+        if (nrComponents == 1) {
+            format = GL_RED;
+        } else if (nrComponents == 3) {
+            format = GL_RGB;
+        } else if (nrComponents == 4) {
+            format = GL_RGBA;
+        }
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        
+    } else {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+    }
+    
+    stbi_image_free(data);
+    
+    return textureID;
+}
+
---
+++ LearnOpenGL/src/4.advanced_opengl/face_culling.hpp
@@ -0,0 +1,14 @@
+//
+//  face_culling.hpp
+//  LearnOpenGL
+//
+//  Created by 秀健身 on 2019/8/22.
+//  Copyright © 2019 秀健身. All rights reserved.
+//
+
+#ifndef face_culling_hpp
+#define face_culling_hpp
+
+int facecull();
+
+#endif /* face_culling_hpp */
